10525eb0e94d907e151923c9ce2407b71b8ff7ae
Edison <iggyboy_guillermo@hotmail.com>
2017-03-26 23:22:34 -0500

    Added more commands (~teemo,~fuck,~roll)

--- NodejsConsoleApp1/NodejsConsoleApp1/app.js
+++ NodejsConsoleApp1/NodejsConsoleApp1/app.js
@@ -3,23 +3,25 @@ var Discord = require('discord.js');
 
 const Events = Discord.Events;
 const client = new Discord.Client();
-/*
-client.connect({
-    token: 'T6Bm_VrskOTMZ0cCXDAPRPBXTetuA2oi'
-});
-*/
+
+function commandIs(str, msg) {
+    return msg.content.startsWith("!"+str)
+}
 client.login('Mjk1NzUwNzIzMTE3OTA3OTY4.C7oW3Q.zNvmGRRMkK6WffH2VyAdAFTwenU');
 
-client.Dispatcher.on(Events.GATEWAY_READY, e => {
-    console.log('Connected as: ' + client.User.Username);
-    });
-
-client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
-    if (e.message.content == 'PING') {
-        e.message.channel.sendMessage('PONG');
-}
+client.on('ready', e => {
+    console.log('Connected :)');
 });
 
+client.on('message', message => {
+    if (commandIs("teemo", message)) {
+        message.reply("Want shrooms, " + message.author.username + "?");
+    }
+    if (commandIs('fuck',message) {
+        message.channel.sendMessage("Dear " + message.author.username + '\n Fuck you in the ass :)');
+    }
+    if (commandIs('roll', message) {
+        message.channel.sendMessage("!role");
+    }
+});
 
-
-//console.log("Hello!");
