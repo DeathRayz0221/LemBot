7f0128d716640227d1e575c61067f3df4aa762f4
Edison <iggyboy_guillermo@hotmail.com>
2017-03-26 22:54:51 -0500

    Adding more stuff on LemBot

--- NodejsConsoleApp1/NodejsConsoleApp1/app.js
+++ NodejsConsoleApp1/NodejsConsoleApp1/app.js
@@ -1,3 +1,25 @@
-'use strict';
 
-console.log('Hello world');
+var Discord = require('discord.js');
+
+const Events = Discord.Events;
+const client = new Discord.Client();
+/*
+client.connect({
+    token: 'T6Bm_VrskOTMZ0cCXDAPRPBXTetuA2oi'
+});
+*/
+client.login('Mjk1NzUwNzIzMTE3OTA3OTY4.C7oW3Q.zNvmGRRMkK6WffH2VyAdAFTwenU');
+
+client.Dispatcher.on(Events.GATEWAY_READY, e => {
+    console.log('Connected as: ' + client.User.Username);
+    });
+
+client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
+    if (e.message.content == 'PING') {
+        e.message.channel.sendMessage('PONG');
+}
+});
+
+
+
+//console.log("Hello!");
