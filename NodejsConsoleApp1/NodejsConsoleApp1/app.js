05bde7f230642396e47f5182c65971a04fac83f6
Edison <iggyboy_guillermo@hotmail.com>
2017-03-26 23:38:33 -0500

    added more functions

--- NodejsConsoleApp1/NodejsConsoleApp1/app.js
+++ NodejsConsoleApp1/NodejsConsoleApp1/app.js
@@ -4,23 +4,38 @@ var Discord = require('discord.js');
 const Events = Discord.Events;
 const client = new Discord.Client();
 
+//Insert functions here 
 function commandIs(str, msg) {
-    return msg.content.startsWith("!"+str)
+    return msg.content.startsWith("~" + str)
 }
 client.login('Mjk1NzUwNzIzMTE3OTA3OTY4.C7oW3Q.zNvmGRRMkK6WffH2VyAdAFTwenU');
 
+function pluck(array) {
+    return array.map(function (item) { return item["name"]; });
+}
+
+function hasRole(mem, role) {
+    if (pluck(mem.role).includes(role)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+//Console
 client.on('ready', e => {
     console.log('Connected :)');
 });
 
+//Bot commands
 client.on('message', message => {
     if (commandIs("teemo", message)) {
-        message.reply("Want shrooms, " + message.author.username + "?");
+        message.channel.sendMessage("Want shrooms, " + message.author.username + "?");
     }
-    if (commandIs('fuck',message) {
+    if (commandIs('fuck',message)) {
         message.channel.sendMessage("Dear " + message.author.username + '\n Fuck you in the ass :)');
     }
-    if (commandIs('roll', message) {
+    if (commandIs('roll', message)) {
         message.channel.sendMessage("!role");
     }
 });
